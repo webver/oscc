51d10f93b99f18f53c259371d58c245c4166750b
webver <webver@gmail.com>
2022-04-06 17:59:36 +0300

    update api

--- api/include/oscc.h
+++ api/include/oscc.h
@@ -13,6 +13,7 @@
 
 #include "can_protocols/brake_can_protocol.h"
 #include "can_protocols/fault_can_protocol.h"
+#include "can_protocols/selector_can_protocol.h"
 #include "can_protocols/steering_can_protocol.h"
 #include "can_protocols/throttle_can_protocol.h"
 #include "vehicles.h"
